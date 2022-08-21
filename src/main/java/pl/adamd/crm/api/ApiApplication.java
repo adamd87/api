1f9c9e9b8dff18231a5c37df8e1cc19f1a01f5bf
adamd87 <adam.drozdz1987@gmail.com>
2022-08-21 23:04:15 +0200

    make it better

--- src/main/java/pl/adamd/crm/api/ApiApplication.java
+++ src/main/java/pl/adamd/crm/api/ApiApplication.java
@@ -1,11 +1,13 @@
 package pl.adamd.crm.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+@SpringBootConfiguration
 public class ApiApplication {
 
 	public static void main(String[] args) {
