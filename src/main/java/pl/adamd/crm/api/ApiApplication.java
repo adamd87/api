c5764cb4f7dba6a10958bd5568862c58573732c9
adamd87 <adam.drozdz1987@gmail.com>
2022-08-21 22:24:45 +0200

    make it better

--- src/main/java/pl/adamd/crm/api/ApiApplication.java
+++ src/main/java/pl/adamd/crm/api/ApiApplication.java
@@ -1,41 +1,14 @@
 package pl.adamd.crm.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import pl.adamd.crm.api.config.employee.entity.ERole;
-import pl.adamd.crm.api.config.employee.entity.Role;
-import pl.adamd.crm.api.config.employee.repository.RoleRepository;
 
 @SpringBootApplication
-public class ApiApplication implements CommandLineRunner {
+public class ApiApplication {
 
-	@Autowired
-	RoleRepository roleRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) {
-		try {
-			if (roleRepository.findAll().isEmpty()) {
-				Role employeeRole = new Role();
-				employeeRole.setName(ERole.ROLE_EMPLOYEE);
-				roleRepository.save(employeeRole);
-				Role adminRole = new Role();
-				adminRole.setName(ERole.ROLE_ADMIN);
-				roleRepository.save(adminRole);
-				Role fitterRole = new Role();
-				fitterRole.setName(ERole.ROLE_FITTER);
-				roleRepository.save(fitterRole);
-			}
-		} catch (Exception ignored) {
-
-		}
-
 	}
 
 }
